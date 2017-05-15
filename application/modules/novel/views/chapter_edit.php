b71c79b31043c81ca5ed1344f205e4f5e1ac1370
iwan safrudin <iwan@fisip.net>
2017-05-15 09:21:02 +0700

    permission edit on publick

--- application/modules/novel/views/chapter_edit.php
+++ application/modules/novel/views/chapter_edit.php
@@ -1,48 +1,53 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-$id = !empty($chapter['id']) ? $chapter['id'] : '';
-if(!empty($msg)&&!empty($alert))
+if(!empty($chapter['editable']))
 {
-	msg($msg,$alert);
+	$id = !empty($chapter['id']) ? $chapter['id'] : '';
+	if(!empty($msg)&&!empty($alert))
+	{
+		msg($msg,$alert);
+	}
+	$this->session->__set('link_js', base_url().'templates/admin/js/bootstrap-tagsinput.js');
+	?>
+	<?php echo form_open(base_url('novel/chapter_edit/'.$id), 'id="chapter_edit"');?>
+		<div class="panel panel-default">
+			<div class="panel panel-heading">
+				<h4 class="panel-title">
+					<?php echo 'Edit Chapter'; ?>
+				</h4>
+			</div>
+			<div class="panel panel-body">
+				<br>
+				<?php
+				echo form_hidden('id',$id);
+				echo form_label('Content', 'content');
+				echo form_textarea(array(
+					'name'  => 'content',
+					'id'    => 'textckeditor',
+					'class' => 'form-control',
+					'value' => @$chapter['content']));
+				?>
+			</div>
+			<div class="panel panel-footer">
+				<?php
+				echo form_button(array(
+	        'id'      => 'submit',
+	        'value'   => 'true',
+	        'type'    => 'success',
+	        'content' => 'submit',
+	        'class'   => 'btn btn-success'));
+				echo form_button(array(
+	        'id'      => 'reset',
+	        'value'   => 'true',
+	        'type'    => 'reset',
+	        'content' => 'reset',
+	        'class'   => 'btn btn-warning'));
+				?>
+			</div>
+		</div>
+		<script type="text/javascript">
+			CKEDITOR.replace('textckeditor');
+		</script>
+	<?php echo form_close();
+}else{
+	msg('anda tidak memiliki hak untuk mengakses halaman ini', 'danger');
 }
-$this->session->__set('link_js', base_url().'templates/admin/js/bootstrap-tagsinput.js');
-?>
-<?php echo form_open(base_url('novel/chapter_edit/'.$id), 'id="chapter_edit"');?>
-	<div class="panel panel-default">
-		<div class="panel panel-heading">
-			<h4 class="panel-title">
-				<?php echo 'Edit Chapter'; ?>
-			</h4>
-		</div>
-		<div class="panel panel-body">
-			<br>
-			<?php
-			echo form_hidden('id',$id);
-			echo form_label('Content', 'content');
-			echo form_textarea(array(
-				'name'  => 'content',
-				'id'    => 'textckeditor',
-				'class' => 'form-control',
-				'value' => @$chapter['content']));
-			?>
-		</div>
-		<div class="panel panel-footer">
-			<?php
-			echo form_button(array(
-        'id'      => 'submit',
-        'value'   => 'true',
-        'type'    => 'success',
-        'content' => 'submit',
-        'class'   => 'btn btn-success'));
-			echo form_button(array(
-        'id'      => 'reset',
-        'value'   => 'true',
-        'type'    => 'reset',
-        'content' => 'reset',
-        'class'   => 'btn btn-warning'));
-			?>
-		</div>
-	</div>
-	<script type="text/javascript">
-		CKEDITOR.replace('textckeditor');
-	</script>
-<?php echo form_close();
--- application/modules/novel/views/novel_edit.php
+++ application/modules/novel/views/novel_edit.php
@@ -1,44 +1,49 @@
 <?php
 $data = $novel;
-$id   = !empty($data['id']) ? $data['id'] : '';
-if(!empty($msg)&&!empty($alert))
+if(!empty($data['editable']))
 {
-	msg($msg,$alert);
+	$id   = !empty($data['id']) ? $data['id'] : '';
+	if(!empty($msg)&&!empty($alert))
+	{
+		msg($msg,$alert);
+	}
+	echo form_open_multipart(base_url('novel/novel_edit/'.$id), 'id="novel_edit"');?>
+		<div class="panel panel-default">
+			<div class="panel panel-heading">
+				<h4 class="panel-title">
+					<?php echo 'Edit Novel'; ?>
+				</h4>
+			</div>
+			<div class="panel panel-body">
+				<?php
+				echo form_hidden('id',$id);
+				echo form_label('description', 'description');
+				echo form_textarea(array(
+					'name'  => 'description',
+					'class' => 'form-control',
+					'rows'  => '4',
+					'value' => @$data['description']));
+				?>
+			</div>
+			<div class="panel panel-footer">
+				<?php
+				echo form_button(array(
+		      'id'      => 'submit',
+		      'value'   => 'true',
+		      'type'    => 'success',
+		      'content' => 'submit',
+		      'class'   => 'btn btn-success'));
+				echo form_button(array(
+		      'id'      => 'reset',
+		      'value'   => 'true',
+		      'type'    => 'reset',
+		      'content' => 'reset',
+		      'class'   => 'btn btn-warning'));
+				?>
+			</div>
+		</div>
+		<?php
+	echo form_close();
+}else{
+	msg('anda tidak memiliki hak untuk mengakses halaman ini', 'danger');
 }
-echo form_open_multipart(base_url('novel/novel_edit/'.$id), 'id="novel_edit"');?>
-	<div class="panel panel-default">
-		<div class="panel panel-heading">
-			<h4 class="panel-title">
-				<?php echo 'Edit Novel'; ?>
-			</h4>
-		</div>
-		<div class="panel panel-body">
-			<?php
-			echo form_hidden('id',$id);
-			echo form_label('description', 'description');
-			echo form_textarea(array(
-				'name'  => 'description',
-				'class' => 'form-control',
-				'rows'  => '4',
-				'value' => @$data['description']));
-			?>
-		</div>
-		<div class="panel panel-footer">
-			<?php
-			echo form_button(array(
-	      'id'      => 'submit',
-	      'value'   => 'true',
-	      'type'    => 'success',
-	      'content' => 'submit',
-	      'class'   => 'btn btn-success'));
-			echo form_button(array(
-	      'id'      => 'reset',
-	      'value'   => 'true',
-	      'type'    => 'reset',
-	      'content' => 'reset',
-	      'class'   => 'btn btn-warning'));
-			?>
-		</div>
-	</div>
-	<?php
-echo form_close();?>
